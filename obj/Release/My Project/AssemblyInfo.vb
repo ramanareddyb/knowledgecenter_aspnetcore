a8f79a1b179d4340f5bb8747433507107035c829
B Ramana Reddy <41152286+ramanareddyb@users.noreply.github.com>
2022-07-12 11:40:12 +0530

    Add files via upload

---
+++ obj/Release/My Project/AssemblyInfo.vb
@@ -0,0 +1,18 @@
+Imports System
+Imports System.Reflection
+Imports System.Runtime.InteropServices
+
+<Assembly: AssemblyTitle("Ajax, Build:LOCAL")>
+<Assembly: AssemblyDescription("Excelicare v0.0, Build:0.0")>
+<Assembly: AssemblyCompany("Practive Health Inc. dba Excelicare")>
+<Assembly: AssemblyProduct("Excelicare")>
+<Assembly: AssemblyCopyright("Copyright 2021 Practive Health Inc. dba Excelicare. All rights reserved.")>
+<Assembly: AssemblyTrademark("Excelicare")>
+
+<Assembly: ComVisible(False)>
+<Assembly: CLSCompliant(True)>
+
+<Assembly: AssemblyVersion("0.0")>
+<Assembly: AssemblyFileVersion("0.0.0.0")>
+<Assembly: AssemblyInformationalVersion("0.0")>
+<Assembly: AssemblyConfiguration("Configuration:Release, Platform:AnyCPU, Build:0.0")>
